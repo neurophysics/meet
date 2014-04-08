afddabe5d6ae1d31e7368a81b520b90b2d6f9528
Gunnar Waterstraat <gunnar.waterstraat@charite.de>
2014-04-08 13:58:03 +0200

    added non-python files to install script

--- setup.py
+++ setup.py
@@ -9,5 +9,6 @@ setup(name='meet',
       author_email='gunnar.waterstraat@gmx.de',
       url='https://github.com/t00tsie/meet',
       packages=['meet'],
+      package_data={'meet': ['plotting_1005.txt', 'test_data/elecnames.txt', 'test_data/sample.dat']},
       install_requires=['numpy', 'scipy', 'matplotlib']
      )
